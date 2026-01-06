424a825f08a09878815a67c4c5beb20e09c46310
Rajan Kumar <rajankbhagat81@gmail.com>
2026-01-06 11:47:16 +0530

    updated files

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -18,7 +18,7 @@ class ApiKeyManager:
 
     def __init__(self):
         self.api_keys = {}
-        raw = os.getenv("API_KEYS")
+        raw = os.getenv("api_keys")
 
         if raw:
             try:
